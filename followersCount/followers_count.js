48d45315d2fcbd471306481725f0c743f111a0f0
Alex-bzh22 <scopalex@hotmail.com>
2025-10-10 09:34:12 -0400

    add peter parker's followers and reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,10 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+
+function resetCount() {
+    count = 0;
+    alert("the Followers count has been reset !");
+    displayCount(); 
+    //document.getElementById('countReset').innerHTML=count; // Display the count in the HTML
+}
